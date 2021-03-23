7af21717067e48f102682efdf2dabe59b5f41442
geophf <dauclair@hotmail.com>
2021-03-23 02:22:28 -0400

    Only downloading the listings file, as it contains the ranking information, too.

--- src/CryptoCoin/CoinMarketCap/scripts/curl-command.sh
+++ src/CryptoCoin/CoinMarketCap/scripts/curl-command.sh
@@ -7,4 +7,4 @@ curl -H "X-CMC_PRO_API_KEY: $COIN_MARKET_CAP_API_KEY" \
      -H "Accept: application/json" \
      -G $ENDPOINT/$1 > $OUT_FILE
 
-echo "Wrote latest rankings to $OUT_FILE."
+echo "Wrote latest listings to $OUT_FILE."
