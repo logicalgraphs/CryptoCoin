8813e1d4fca9ccf3814a62c81719c534e173194b
geophf <dauclair@hotmail.com>
2021-03-24 11:38:41 -0400

    Updated the report.sh-script to run an expect script to run Go.go.

--- src/CryptoCoin/CoinMarketCap/scripts/report.sh
+++ src/CryptoCoin/CoinMarketCap/scripts/report.sh
@@ -6,9 +6,9 @@ LIST_CMD="listings/latest?start=1&limit=5000&convert=USD"
 
 CURL_CMD=$COIN_MARKET_CAP_DIR/scripts/curl-command.sh
 
-cd $COIN_MARKET_CAP_DIR/scripts
 $CURL_CMD cryptocurrency/$LIST_CMD $LISTING_FILE
 
-runhaskell $COIN_MARKET_CAP_DIR/scripts/report.hs
+cd $CRYPTOCOIN_DIR
+$COIN_MARKET_CAP_DIR/scripts/run-report.exp
 
 echo "done."
