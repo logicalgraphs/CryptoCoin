85c3d71f2655c823e8d6ff4b28107c542e05c7c3
geophf <dauclair@hotmail.com>
2021-04-13 06:11:56 -0400

    Completed On Balance Volume (OBV)

--- src/CryptoCoin/CoinMarketCap/Analytics/Trends/OnBalanceVolume.hs
+++ src/CryptoCoin/CoinMarketCap/Analytics/Trends/OnBalanceVolume.hs
@@ -1,20 +1,26 @@
 module CryptoCoin.CoinMarketCap.Analytics.Trends.OnBalanceVolume where
 
-
 {--
 Calculating OBV
-On-balance volume provides a running total of an asset's trading volume and indicates whether this volume is flowing in or out of a given security or currency pair. The OBV is a cumulative total of volume (positive and negative). There are three rules implemented when calculating the OBV. They are:
+On-balance volume provides a running total of an asset's trading volume and 
+indicates whether this volume is flowing in or out of a given security or 
+currency pair. The OBV is a cumulative total of volume (positive and negative). 
+There are three rules implemented when calculating the OBV. They are:
 
-1. If today's closing price is higher than yesterday's closing price, then: Current OBV = Previous OBV + today's volume
+1. If today's closing price is higher than yesterday's closing price, then: 
+Current OBV = Previous OBV + today's volume
 
 
-2. If today's closing price is lower than yesterday's closing price, then: Current OBV = Previous OBV - today's volume
+2. If today's closing price is lower than yesterday's closing price, then: 
+Current OBV = Previous OBV - today's volume
 
 
-3. If today's closing price equals yesterday's closing price, then: Current OBV = Previous OBV
+3. If today's closing price equals yesterday's closing price, then: 
+Current OBV = Previous OBV
 
 Example Of How To Use On-Balance Volume
-Below is a list of 10 days' worth of a hypothetical stock's closing price and volume:
+Below is a list of 10 days' worth of a hypothetical stock's closing price and 
+volume:
 
 Day one: closing price equals $10, volume equals 25,200 shares
 Day two: closing price equals $10.15, volume equals 30,000 shares
@@ -26,7 +32,12 @@ Day seven: closing price equals $10.20, volume equals 36,000 shares
 Day eight: closing price equals $10.20, volume equals 20,500 shares
 Day nine: closing price equals $10.22, volume equals 23,000 shares
 Day 10: closing price equals $10.21, volume equals 27,500 shares
-As can be seen, days two, three, six, seven and nine are up days, so these trading volumes are added to the OBV. Days four, five and 10 are down days, so these trading volumes are subtracted from the OBV. On day eight, no changes are made to the OBV since the closing price did not change. Given the days, the OBV for each of the 10 days is:
+
+As can be seen, days two, three, six, seven and nine are up days, so these 
+trading volumes are added to the OBV. Days four, five and 10 are down days, so 
+these trading volumes are subtracted from the OBV. On day eight, no changes are 
+made to the OBV since the closing price did not change. Given the days, the OBV 
+for each of the 10 days is:
 
 Day one OBV = 0
 Day two OBV = 0 + 30,000 = 30,000
@@ -41,3 +52,24 @@ Day 10 OBV = 99,600 - 27,500 = 72,100
 
 https://www.investopedia.com/terms/o/onbalancevolume.asp
 --}
+
+import Control.Logic.Frege ((<<-))
+
+import Data.CryptoCurrency.Types (row)
+import Data.CryptoCurrency.Types.PriceVolume (PriceVolume, vol)
+import Data.CryptoCurrency.Types.Vector (Vector, vals)
+
+obvi :: ((trend, Maybe double), Vector PriceVolume) -> Double
+obvi ((_trend, _mbOBV), v) = 
+   foldr (+) 0 (computeSigns . reverse . map (vol . row) $ vals v)
+
+computeSigns :: [Double] -> [Double]
+computeSigns vols = 
+   zipWith 
+   (\a b -> plusMinus (compare a b) a b)
+   (0:vols) vols
+
+plusMinus :: Ordering -> Double -> Double -> Double
+plusMinus LT a b = (-1) * b
+plusMinus GT _ b = b
+plusMinus EQ _ _ = 0
