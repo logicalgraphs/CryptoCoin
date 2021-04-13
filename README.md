183a2d8b0774704be389e5c74fac87a3c06ad8e0
geophf <dauclair@hotmail.com>
2021-04-13 07:46:32 -0400

    Added runAllIndicatorsOns [sic]: I can see all indicators on selected symbols.

--- README.md
+++ README.md
@@ -107,6 +107,9 @@ https://github.com/geophf/1HaskellADay/tree/master/exercises/HAD/Graph/D3
 ## Revision History
 ### ... or feeping creatures! AHA!
 
+* 2021-04-13: Finished On Balance Volume, switched to list-monad for
+trend-processing.
+
 * 2021-04-11: Relative Strength Index
 * 2021-04-09: Moving Averages convergence/divergence (macd)
 * 2021-04-07: using trends for trend-computations. Implemented ema (Exponential
--- src/CryptoCoin/CoinMarketCap/Analytics/Trends/Indicators.hs
+++ src/CryptoCoin/CoinMarketCap/Analytics/Trends/Indicators.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
 module CryptoCoin.CoinMarketCap.Analytics.Trends.Indicators where
 
@@ -6,9 +7,6 @@ module CryptoCoin.CoinMarketCap.Analytics.Trends.Indicators where
 Like the Candlestick patterns, these are the "Trend Trading: The 4 Common
 Indicators" outside the candlesticks (focusing more on price, ... sometimes).
 
---}
-
-{--
 Need:
 
 cmc_id, volume_24h, for_date, quote_price
@@ -17,7 +15,7 @@ Need: 50 an 200 for sma, 12 and 26 for EMA / MACD, 15 for RSI,
 OBV just needs the previous day and today.
 --}
 
-import Control.Arrow ((&&&))
+import Control.Arrow ((&&&), first)
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -33,10 +31,12 @@ import CryptoCoin.CoinMarketCap.Analytics.Trends.MovingAverageConvergenceDiverge
 import CryptoCoin.CoinMarketCap.Analytics.Trends.RelativeStrengthIndex (rsi)
 import CryptoCoin.CoinMarketCap.Analytics.Trends.OnBalanceVolume (obvi)
 
-import Data.CryptoCurrency.Types (row)
+import Data.CryptoCurrency.Types (row, Idx)
 import Data.CryptoCurrency.Types.PriceVolume
 import Data.CryptoCurrency.Types.Trend
 import Data.CryptoCurrency.Types.Vector (Vector, vtake)
+
+import Data.LookupTable
 
 import Store.SQL.Connection (withConnection, Database(ECOIN))
 
@@ -89,17 +89,62 @@ guardedIndicator :: PVdom -> Int -> IndicatorA -> Maybe Double
 guardedIndicator (t, v) sz (nf, (tf, f)) =
    f <$> sequence ((id &&& tf . row) t, vtake (nf sz) v)
 
-btc :: Indicator -> Int -> IO ()
-btc = btc' [T temporal, E eternal]
+indies :: [Dependency]
+indies = [T temporal, E eternal]
 
-btc' :: LookInto indicators => [indicators] -> Indicator -> Int -> IO ()
-btc' indies ind i =
+type RunCoin = Indicator -> Int -> IO ()
+
+btc, bnb, eth :: RunCoin  -- the big three
+btc = c' 1
+eth = c' 1027
+bnb = c' 1839
+
+c' :: Idx -> Indicator -> Int -> IO ()
+c' = coin' indies
+
+coin' :: LookInto indicators => [indicators] -> Idx -> Indicator -> Int -> IO ()
+coin' indies idx ind i =
    let mbIndA = mapMaybe (flip lookin (ind, i)) indies
        mbG v = mbIndA >>= maybeToList . guardedIndicator v i
-   in  withConnection ECOIN (\conn ->
-          fetchLastTrend conn 1     >>= \trend ->
-          fetchPricesVolumes conn 1 >>= \pv ->
-          print (mbG (trend, pv)))
+   in  withConnection ECOIN (\conn -> fetchDomain conn idx >>= print . mbG)
+
+fetchDomain :: Connection -> Integer -> IO PVdom
+fetchDomain conn idx =
+   (,) <$> fetchLastTrend conn idx <*> fetchPricesVolumes conn idx
+
+fetchCmcIdsQuery :: Query
+fetchCmcIdsQuery = "SELECT symbol, cmc_id FROM coin WHERE symbol in ?"
+
+fetchCmcIds :: Connection -> [String] -> IO LookupTable
+fetchCmcIds conn coins =
+   Map.fromList <$> query conn fetchCmcIdsQuery (Only (In coins))
+
+runAllIndicatorsOns :: [String] -> IO ()
+runAllIndicatorsOns coins =
+   withConnection ECOIN (\conn ->
+      fetchCmcIds conn coins >>= mapM_ (runAllIndicatorsOn conn) . Map.toList)
+
+runAllIndicatorsOn :: Connection -> (String, Integer) -> IO ()
+runAllIndicatorsOn conn (sym, idx) =
+   putStrLn ("For e-coin " ++ sym ++ " (CMC ID: " ++ show idx ++ "):") >>
+   fetchDomain conn idx >>=
+   flip mapM_ (pair indies) . runIndicator
+
+runIndicator :: PVdom -> ((Indicator, Int), IndicatorA) -> IO ()
+runIndicator dom ((ind, i), r) =
+   let res = guardedIndicator dom i r in
+   putStrLn ("   " ++ show ind ++ " (" ++ show i ++ "): " ++ mbshow res)
+
+mbshow :: Show a => Maybe a -> String
+mbshow Nothing = "--"
+mbshow (Just a) = show a
+
+pair :: [Dependency] -> [((Indicator, Int), IndicatorA)]
+pair = concat . map (p' 1)
+
+p' :: Int -> Dependency -> [((Indicator, Int), IndicatorA)]
+p' _ (T (Temporal t)) = Map.toList t
+p' i (E (Eternal e)) = map (first (,i)) (Map.toList e)
 
 {--
 >>> btc ExponentialMovingAverage 12
@@ -122,4 +167,35 @@ Bitcoin is just chugging along, isn't it.
 [-8.32027697067179e10]
 
 But may be oversold?
+
+And, to run all the indicators:
+
+>>> runAllIndicatorsOns (words "BTC ETH BNB")
+For e-coin BNB (CMC ID: 1839):
+   SimpleMovingAverage (50): --
+   SimpleMovingAverage (200): --
+   ExponentialMovingAverage (9): 441.1934542263441
+   ExponentialMovingAverage (12): 414.90682992567207
+   ExponentialMovingAverage (26): 339.2260297583855
+   MovingAverageConvergenceDivergence (1): 81.82557999904827
+   RelativeStrengthIndex (1): 72.4312195763753
+   OnBalanceVolume (1): -2.684611258097431e10
+For e-coin BTC (CMC ID: 1):
+   SimpleMovingAverage (50): --
+   SimpleMovingAverage (200): --
+   ExponentialMovingAverage (9): 59097.80574823082
+   ExponentialMovingAverage (12): 59134.597811008054
+   ExponentialMovingAverage (26): 57798.71974225892
+   MovingAverageConvergenceDivergence (1): 1411.982019920164
+   RelativeStrengthIndex (1): 48.53384918758772
+   OnBalanceVolume (1): -8.32027697067179e10
+For e-coin ETH (CMC ID: 1027):
+   SimpleMovingAverage (50): --
+   SimpleMovingAverage (200): --
+   ExponentialMovingAverage (9): 2110.11014698635
+   ExponentialMovingAverage (12): 2068.89789499771
+   ExponentialMovingAverage (26): 1906.5550178257406
+   MovingAverageConvergenceDivergence (1): 178.09325184955037
+   RelativeStrengthIndex (1): 63.44754316660384
+   OnBalanceVolume (1): -2.4423049124036507e10
 --}
