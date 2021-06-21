dc5800f5d7e9c162e7e0b0d614c31b56c4f95223
geophf <dauclair@hotmail.com>
2021-06-21 08:26:04 -0400

    Adding CoinTransfer type

--- src/Data/CryptoCurrency/Types/Transfers/Internal.hs
+++ src/Data/CryptoCurrency/Types/Transfers/Internal.hs
@@ -18,16 +18,32 @@ import Data.Monetary.USD
 
 ----- Handling Cash transfers ------------------------------------------------
 
-data CashTransF' = Cxf Day USD Idx Idx
+data CashTransF' = Cshxf Day USD Idx Idx
    deriving (Eq, Ord, Show)
 
 instance ToRow CashTransF' where
-   toRow (Cxf d a p dir) = [toField d, toField p, toField dir, toField a]
+   toRow (Cshxf d a p dir) = [toField d, toField p, toField dir, toField a]
 
-storeTransFQuery :: Query
-storeTransFQuery = Query . B.pack $ unwords [
+storeCashTransFQuery :: Query
+storeCashTransFQuery = Query . B.pack $ unwords [
    "INSERT INTO transfer_funds (for_date, portfolio_id, transfer_direction_id,",
    "amount) VALUES (?, ?, ?, ?)"]
 
 storeCashTransF' :: Connection -> [CashTransF'] -> IO ()
-storeCashTransF' conn = void . executeMany conn storeTransFQuery
+storeCashTransF' conn = void . executeMany conn storeCashTransFQuery
+
+----- Now: coin transfers ---------------------------------------------------
+
+                      --  dt  amt    coin surcharge from to
+data CoinTransF' = Cnxf Day Double Idx Double Idx Idx
+   deriving (Eq, Ord, Show)
+
+instance ToRow CoinTransF' where
+   toRow (Cnxf dt amt cn sur frm to) =
+      [toField dt, toField amt, toField cn,
+       toField sur, toField frm, toField to]
+
+storeCoinTransFQuery :: Query
+storeCoinTransFQuery = Query . B.pack $ unwords [
+   "INSERT INTO transfer_coin (for_date,amount,cmc_id,surcharge,transfer_from,",
+   "transfer_to) VALUES (?, ?, ?, ?, ?, ?)"]
