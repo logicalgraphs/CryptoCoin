b069f398ebb39df04c533d98c675f239d2f6eb39
geophf <dauclair@hotmail.com>
2021-07-26 05:28:50 -0400

    Added coin-transfer computations to portfolio reports.

--- src/CryptoCoin/CoinMarketCap/Reports/Portfolio.hs
+++ src/CryptoCoin/CoinMarketCap/Reports/Portfolio.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 module CryptoCoin.CoinMarketCap.Reports.Portfolio where
 
@@ -10,6 +11,15 @@ Woo, boy!
 1.1: First do the transactions (DONE), then add and subtract each coin transfer
      from-portfolio = coins - amt xfered
      to-portfolio   = coins + (amt - surcharge) xfered
+
+To incorporate the coin transfer (really, exchange transfer), we need to 
+restructure the PorfolioReport-type:
+
+Map Name PortfolioReport  (portfolioName -> PortfolioReport)
+
+And the holdings type from Set Holding to
+
+Map Idx Holding (coinId -> Holding)
 --}
 
 import Control.Arrow ((&&&))
@@ -23,25 +33,35 @@ import Data.Maybe (mapMaybe)
 import Data.Monoid (mconcat)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Time (Day, addDays)
+import Data.Time (Day)
 
-import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple (Connection)
 
 import Control.Logic.Frege ((-|))
 import Control.Presentation hiding (S)
 import CryptoCoin.CoinMarketCap.Reports.Table hiding (ecoin)
 import CryptoCoin.CoinMarketCap.Types
-import CryptoCoin.CoinMarketCap.Types.Quote
+       (Listings, ECoin, fetchListings, coin, quote)
+import CryptoCoin.CoinMarketCap.Types.Quote (price)
 
 import Data.CryptoCurrency.Types
+       (Indexed, Rank, rank, Idx, idx, namei, sym, IxRow(IxRow))
 import Data.CryptoCurrency.Types.Coin (allCoinsLk)
 import Data.CryptoCurrency.Types.Portfolio
+       (Portfolio(Portfolio), fetchPortfolii, portfolioName, fetchCoinIdsFor,
+        cash, Portfolii)
+import Data.CryptoCurrency.Types.Recommendation (Call(BUY))
 import Data.CryptoCurrency.Types.Transaction
+       (Transaction(Transaction), fetchTransactionsByPortfolio, spent, ncoins)
 import Data.CryptoCurrency.Types.Transactions.Context
+       (TransactionContext, transContext)
+import Data.CryptoCurrency.Types.Transfers.Coin
+       (fetchCoinTransfers, CoinTransferDatum(CoinTransferDatum), CoinTransfer)
 import Data.CryptoCurrency.Utils (pass)
 import Data.LookupTable (LookupTable)
-import Data.Monetary.USD
-import Data.Percentage
+import Data.Monetary.USD (USD(USD), doubledown)
+import Data.Percentage (Percentage(P))
+import Data.XHTML (Name)
 
 import Store.SQL.Connection (withConnection, Database(ECOIN))
 import Store.SQL.Util.Indexed (IxValue(IxV))
@@ -66,14 +86,15 @@ instance Univ Holding where
       [show (rank h), show (idx h), sym e, namei e, show amt, show ui, show up,
        show ave, show (USD currVal), show (change inv currVal)]
 
-data PortfolioReport = PR { portfolio :: Portfolio, holdings :: Set Holding }
-   deriving (Eq, Ord, Show)
+data PortfolioReport =
+   PR { portfolio :: Portfolio, holdings :: Map Idx Holding }
+      deriving (Eq, Ord, Show)
 
 instance Monoid PortfolioReport where
-   mempty = PR (Portfolio "Nada" (USD 0) Nothing) Set.empty
+   mempty = PR (Portfolio "Nada" (USD 0) Nothing) Map.empty
    PR p0 h0 `mappend` PR p1 h1 =
        PR (Portfolio "All Portfolii" (cash p0 + cash p1) Nothing)
-          (Set.union h0 h1)
+          (Map.union h0 h1)
 
 totalInvested, totalValue :: PortfolioReport -> USD
 totalInvested (PR _ holds) = sumOver invested holds
@@ -106,7 +127,8 @@ data KV = KV String String
 instance Univ KV where explode (KV k v) = [k, v]
 
 portHdrs :: [String]
-portHdrs = words "Rank Id Symbol Coin Amount Invested Price Ave Value %change"
+portHdrs =
+   words "Rank Id Symbol Coin Amount Invested Price Ave Value %change call"
 
 portTitle :: Portfolio -> String
 portTitle p = "Holdings for " ++ portfolioName p ++ " portfolio"
@@ -116,9 +138,9 @@ printPortfolioCSV date pr@(PR p holdings) =
    let cr = putStrLn "" in
    cr                         >>
    summarizePortfolio date pr >>
-   if holdings == Set.empty then return ()
+   if holdings == Map.empty then return ()
    else cr                    >>
-   csvReport' portHdrs (portTitle p) (sortOn rank (Set.toList holdings))
+   csvReport' portHdrs (portTitle p) (sortOn rank (Map.elems holdings))
 
 data PorNot = Perc Percentage | PNOT
    deriving Eq
@@ -142,14 +164,112 @@ forEachPortfolioDo conn tday tc (prs, lists) i@(IxV ix port) =
    let combinedListings = Map.union newLists lists
        coinsHeld = mapMaybe (uncurry (toHolding combinedListings))
                             (Map.toList transs)
-   in  return (PR port (Set.fromList coinsHeld):prs, combinedListings)
+   in  return (PR port (Map.fromList $ map (idx &&& id) coinsHeld):prs,
+               combinedListings)
+
+{--
+Now let's roll in the coin-transfers. Now, we have to be smart about this,
+because we have the coin-transfers, but we also have the coin-conversions,
+both of which are date-sensitive. That means we have to go to the coin-
+conversion first, get the first date, then execute all the transfers that 
+happen before that date, THEN execute the coin-conversion, lather, rinse, 
+repeat until we run out of coin-conversions, then finish off the remaining
+coin-transfers.
+
+Let's see what the coin-transfers look like:
+
+>>> import Database.PostgreSQL.Simple
+>>> import Store.SQL.Connection 
+>>> conn <- connectInfo ECOIN >>= connect
+>>> pLk <- portfoliiLk conn
+>>> xfers <- fetchCoinTransfers conn pLk
+>>> length xfers
+40
+>>> take 2 xfers
+[IxRow 5692 2021-07-06
+       (CoinTransferDatum {amount = 0.12445026, surcharge = 7.3138e-3,
+                           basis = $0.00, xfrom = "COINBASE", xto = "GEMINI"}),
+ IxRow 1 2021-06-18 (CoinTransferDatum {amount = 2.83418e-3, 
+                                        surcharge = 1.025e-5, basis = $100.00,
+                                        xfrom = "COINBASE", xto = "PHEMEX"})]
+
+lolneet! ... but this also means we want to sort these pupperz by date: DONE!
+
+[IxRow 1 2021-06-18
+         (CoinTransferDatum {amount = 3.66589e-3, surcharge = 5.7e-4,
+                             basis = $150.00, 
+                             xfrom = "PHEMEX", xto = "COINBASE"})
+ IxRow 1 2021-06-18
+         (CoinTransferDatum {amount = 2.83418e-3, surcharge = 1.025e-5,
+                             basis = $100.00,
+                             xfrom = "COINBASE", xto = "PHEMEX"})]
+
+Voilà. Now we know what to do, ... at least for the coin-transfer-part.
+--}
+
+type PortfolioReports = Map Name PortfolioReport
+
+transferCoins :: Connection -> Portfolii -> ([PortfolioReport], Listings)
+              -> IO PortfolioReports
+transferCoins conn (Map.map idx -> pLk) (reports, listings) =
+   let toMap = Map.fromList . map (portfolioName . portfolio &&& id) in
+   putStrLn "Transferring coins between portfolii" >>
+   fetchCoinTransfers conn pLk                     >>=
+   foldM (transferCoin listings) (toMap reports)
+
+data Haben = REQUIRED | OPTIONAL
+   deriving Eq
+
+transferCoin :: Listings -> PortfolioReports -> CoinTransfer
+             -> IO PortfolioReports
+transferCoin l m x@(IxRow cid _dt (CoinTransferDatum amt sur bas fr xto)) =
+   maybe (error ("Could not record transfer " ++ show x)) return
+         (adjustHolding m fr l cid (-1 * amt) (-1 * bas) REQUIRED >>= \m1 ->
+          adjustHolding m1 xto l cid (amt - sur) bas OPTIONAL)
+
+adjustHolding :: PortfolioReports -> Name -> Listings -> Idx -> Double -> USD
+              -> Haben -> Maybe PortfolioReports
+adjustHolding reps port = ah' port reps (Map.lookup port reps)
+
+ah' :: Name -> PortfolioReports -> Maybe PortfolioReport -> Listings -> Idx
+    -> Double -> USD -> Haben -> Maybe PortfolioReports
+ah' n _ Nothing _ _ _ _ _ = error ("Could not find portfolio named " ++ n)
+ah' port reps (Just pr) listings cid amt bas hab =
+   let hs = holdings pr in
+   ah'' listings port reps pr hs (Map.lookup cid hs) cid amt bas hab
+
+ah'' :: Listings -> Name -> PortfolioReports -> PortfolioReport
+     -> Map Idx Holding -> Maybe Holding -> Idx -> Double -> USD -> Haben
+     -> Maybe PortfolioReports
+ah'' _ port _ _ _ Nothing cid _ _ REQUIRED =
+   error ("No holdings for coin #" ++ show cid ++ " for portfolio " ++ port)
+ah'' listings port reps pr hs Nothing cid amt bas OPTIONAL =
+   let transs = [Transaction "" undefined bas undefined amt BUY ""] in
+   updateHolding port reps pr hs cid <$> toHolding listings cid transs
+ah'' _ port reps pr hs (Just h@(Holding _ am inv _)) cid amt bas _ =
+   Just (updateHolding port reps pr hs cid
+                       (h { amount = am + amt, invested = inv + bas }))
+
+updateHolding :: Name -> PortfolioReports -> PortfolioReport -> Map Idx Holding
+              -> Idx -> Holding -> PortfolioReports
+updateHolding port reps pr hs cid newhold =
+   Map.insert port (pr { holdings = Map.insert cid newhold hs }) reps
+
+cleanPortfolii :: PortfolioReports -> [PortfolioReport]
+cleanPortfolii = map cleanPortfolio . Map.elems
+
+cleanPortfolio :: PortfolioReport -> PortfolioReport
+cleanPortfolio pr = let hs = holdings pr in pr { holdings = cleanHoldings hs }
+
+cleanHoldings :: Map Idx Holding -> Map Idx Holding
+cleanHoldings = Map.filter ((> 0) . amount)
 
 buildSummary :: PortfolioReport -> [KV]
 buildSummary pr@(PR (Portfolio name reserve _) holdings) =
    let ti@(USD totInv) = totalInvested pr
        tv@(USD totVal) = totalValue pr in
    [KV "Cash reserve" (show reserve)]
-   ++ if holdings == Set.empty then []
+   ++ if holdings == Map.empty then []
       else [KV "Total invested" (show ti),
             KV "Current value" (show tv),
             KV "Gain/Loss" (show $ change totInv totVal)]
@@ -161,11 +281,17 @@ summarizePortfolio date pr@(PR (Portfolio name _ _) holdings) =
 summarizePortfolii :: Day -> [PortfolioReport] -> IO ()
 summarizePortfolii date = summarizePortfolio date . mconcat
 
+realizePortfolioReports :: Connection -> Day -> Portfolii
+                        -> IO ([PortfolioReport], Listings)
+realizePortfolioReports conn date ports =
+   transContext conn                                          >>= \tc ->
+   foldM (forEachPortfolioDo conn date tc) ([], Map.empty) (Map.elems ports)
+
 go :: IO ()
 go = withConnection ECOIN (\conn ->
-        latest conn "coin_market_cap_daily_listing" "for_date"      >>= \tday ->
-        transContext conn                                             >>= \tc ->
-        fetchPortfolii conn                                                  >>=
-        foldM (forEachPortfolioDo conn tday tc) ([], Map.empty) . Map.elems  >>=
-        pass (summarizePortfolii tday) . fst                                 >>=
+        latest conn "coin_market_cap_daily_listing" "for_date"     >>= \tday ->
+        fetchPortfolii conn                                        >>= \ports ->
+        realizePortfolioReports conn tday ports                    >>=
+        transferCoins conn ports                                   >>=
+        pass (summarizePortfolii tday) . cleanPortfolii            >>=
         mapM_ (printPortfolioCSV tday))
