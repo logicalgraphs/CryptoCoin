1745c954a18657eaddc06639c1bd9fade5ce23eb
geophf <dauclair@hotmail.com>
2021-09-12 21:12:50 -0400

    Updated hodling SQL for claimed rewards and added ($,%) for readability

--- data-files/sqls/hodlings.sql
+++ data-files/sqls/hodlings.sql
@@ -1,8 +1,13 @@
 -- computes hodlings by coin, ordered by percentage
 
+-- now includes symbols ($,%) to ease the eyes
+-- now includes claimed rewards from staked coins
+
 with trans as (select t.cmc_id as idx,c.symbol as sym,t.n_coins as n,
 t.purchase_usd as invested,
-(case when t.call_id=1 then 1 else -1 end) as mult
+(case when t.call_id=1 then 1
+when t.call_id=4 then 1
+else -1 end) as mult
 from transaction_log t
 inner join coin c on c.cmc_id=t.cmc_id),
 tots as (select idx,sym, sum(n*mult) as amt,sum(invested*mult) as inv
@@ -17,11 +22,12 @@ where p.for_date=(select max(for_date) from coin_market_cap_daily_listing)),
 values as (select date,idx,sym,amt,price,amt*price as usd_value,inv
 from prices),
 portfolios as
-(select date,sym,amt,price,usd_value,inv,
+(select date,idx,sym,amt,price,usd_value,inv,
 (select sum(usd_value) from values) as alles
 from values
-group by date,sym,amt,price,usd_value,inv)
-select date,sym,amt,price,usd_value,(usd_value / alles) as percentage,
-inv as invested,usd_value - inv as "gain/loss",
-(case when inv > 1 then cast(((usd_value - inv) / inv) as text) else 'N/A' end) as "gain (%)"
+group by date,sym,idx,amt,price,usd_value,inv)
+select date,idx,sym,amt,'$' || price as price, '$' || usd_value as usd_value,(usd_value / alles) || '%' as percentage,
+'$' || inv as invested,'$' || (usd_value - inv) as "gain/loss",
+(case when inv > 1 then ((usd_value - inv) / inv) || '%' else 'N/A' end) as "gain (%)"
 from portfolios
+order by (usd_value / alles) desc
